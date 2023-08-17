2e8bb7c5885c98f571ef5c921805dbae5622ec8e
Kowalska Marta <kamarta0991@gmail.com>
2023-08-17 22:29:25 +0200

    task 02 - done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
+import Vimeo from '@vimeo/player';
 
+const CURRENT_TIME = "videoplayer-current-time";
+const THROTTLE_TIME = 1000;
+
+const vimeoPlayer = document.querySelector('#vimeo-player');
+const player = new Vimeo(vimeoPlayer);
+
+player.setCurrentTime(parseInt(localStorage.getItem(CURRENT_TIME), 10));
+
+const onPlay = currentTime => {
+    player.getEnded().then(end => {
+        if (end){
+            console.log('Koniec filmu');
+            localStorage.setItem(CURRENT_TIME, 0);
+        }
+        else {
+            console.log('Film nadal się odtwarza');
+            localStorage.setItem(CURRENT_TIME, parseInt(currentTime.seconds, 10));
+        }
+    })
+}
+
+player.on('timeupdate', throttle(onPlay, THROTTLE_TIME));
